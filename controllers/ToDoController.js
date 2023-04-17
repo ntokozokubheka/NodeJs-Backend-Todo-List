59e9d9594f09d7085f26e1cf07f6d1b6ecde14a0
Stokza <37531253+ntokozokubheka@users.noreply.github.com>
2023-04-17 15:22:27 +0200

    Add files via upload

---
+++ controllers/ToDoController.js
@@ -0,0 +1,19 @@
+const ToDoModel = require("../models/ToDoModel");
+
+module.exports.getToDo = async (req, res) => {
+
+  const todo = await ToDoModel.find();
+    res.send(todo);
+}
+
+module.exports.saveToDo = (req, res) => {
+    const { text } = req.body;
+    ToDoModel
+        .create({ text })
+        .then((data) =>{ 
+            console.log("Added Successfully...")
+            console.log(data)
+            res.send(data)
+        })
+        .catch((err) => console.log(err));
+}
